b6dd0d44022d4e3ec72c091deb7a30bfed872829
Yordan Miladinov <yordan@4web.bg>
2013-03-18 01:58:35 +0200

    add simple tests for future tasks

---
+++ futuretest.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import time
+import unittest
+
+from monitor import Future
+
+
+class FutureTest(unittest.TestCase):
+
+    def test_blocking(self):
+        def worker(arg):
+            time.sleep(2)
+            return arg            
+
+        begin = time.time()
+        f = Future(worker, 2, 1000)
+        actual = f()
+        end = time.time()
+
+        self.assertLessEqual(2, end - begin)
+        return self.assertEqual(1000, f())
+
+    def test_cancel(self):
+        is_executed = False
+
+        def worker():
+            nonlocal is_executed
+            is_executed = True
+        
+        f = Future(worker, 2)
+        time.sleep(1)
+        f.cancel()
+
+        self.assertIsNone(f())
+        self.assertFalse(is_executed)
+
+
+if __name__ == '__main__':
+    unittest.main()
