06a20e31754561d7424c22081fc3a474405cf003
Yordan Miladinov <yordan@4web.bg>
2013-03-18 14:53:18 +0200

    Add comments and a new test case

--- futuretest.py
+++ futuretest.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from __future__ import unicode_literals
-
 import time
 import unittest
 
@@ -16,12 +14,12 @@ class FutureTest(unittest.TestCase):
             return arg            
 
         begin = time.time()
-        f = monitor.Future(worker, 2, 1000)
+        f = monitor.Future(worker, 2, 0xdeadbeef)
         actual = f()
         end = time.time()
 
         self.assertLessEqual(2, end - begin)
-        return self.assertEqual(1000, f())
+        return self.assertEqual(0xdeadbeef, f())
 
     def test_cancel(self):
         is_executed = False
@@ -29,13 +27,58 @@ class FutureTest(unittest.TestCase):
         def worker():
             nonlocal is_executed
             is_executed = True
-        
-        f = monitor.Future(worker, 2)
-        time.sleep(1)
-        f.cancel()
+            return 0xdeadbeef
 
+        # The future task will wait 2 seconds before it starts
+        f = monitor.Future(worker, 2)
+
+        # Thus a second after the start the task should still be
+        # cancel-able.
+        time.sleep(1)
+
+        # cancel() should return True when the cancellation is
+        # successful
+        self.assertTrue(f.cancel())
+
+        self.assertFalse(f.cancel())
+
+        # Assert f() doesn't block
+        begin = time.time()
         self.assertIsNone(f())
+        end = time.time()
+        self.assertLess(end - begin, 0.1)
+
+        # Assert the task function isn't executed
         self.assertFalse(is_executed)
+
+    def test_cancel2(self):
+        is_executed = False
+
+        def worker():
+            nonlocal is_executed
+            time.sleep(2)
+            is_executed = True
+            return 0xdeadbeef
+
+        # The future task will wait for a second before it starts the
+        # worker
+        f = monitor.Future(worker, 1)
+
+        # The worker takes at least 2 seconds to execute.  Thus
+        # currently it's being executed and a cancellation is
+        # impossible.
+        time.sleep(2)
+        self.assertFalse(f.cancel())
+
+        # Since there is roughly a second left for the future task to
+        # complete, assert that time is greater than 0.9s
+        begin = time.time()
+        self.assertEqual(0xdeadbeef, f())
+        end = time.time()
+        self.assertLessEqual(0.9, end - begin)
+
+        # Assert the task function was executed
+        self.assertTrue(is_executed)
 
 
 if __name__ == '__main__':
